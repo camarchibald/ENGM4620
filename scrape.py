f88e29f5dcf0e4c2e82ad11d9636374f8dc39d00
Cameron Archibald <camarchibald@dal.ca>
2025-02-21 00:13:06 -0400

    Save and pickle data from all listings in Halifax

--- scrape.py
+++ scrape.py
@@ -1,37 +1,100 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import re
+import pickle
 
 
 # Get URL based on page number TODO allow different cities to be inputted
-def get_url(pg):
+def get_url(pg) -> str:
     return f"https://www.realtor.ca/map#view=list&CurrentPage={pg}&Sort=6-D&GeoIds=g30_dxgnyskn&GeoName=Halifax%2C%20NS&PropertyTypeGroupID=1&TransactionTypeId=2&PropertySearchTypeId=1&Currency=CAD&HiddenListingIds=&IncludeHiddenListings=false"
 
+# Read max number of pages
+def find_num_pages(sp: BeautifulSoup) -> int:
+    # TODO use rstrip
+    return int(re.sub(r'\+', '',(sp.find_all('span', attrs={'class': 'paginationTotalPagesNum'})[2]).text))
 
-# Open driver on first page
-driver = webdriver.Chrome()
-driver.get(url=get_url(pg=1))
-pass  # Breakpoint here to pass captcha TODO dynamically detect when captcha completed
+class CityData:
+    def __init__(self, name: str):
+        self.city_name = name
+        self.house_data = []
 
-count = 1  # How many houses have been found
+    # TODO prefer to use dataframe rather than array of dictionaries, no need to store keys 600 times
+    def add_house(self, address: str, price: str):
+        self.house_data.append({'address': address, 'price': int(re.sub(r'[$|,]', '', price))})
 
-# Go through pages TODO read the max number of pages at the start and search all of it
-for page in range(1, 11):
-    driver.get(url=get_url(pg=page))
-    driver.refresh()
-    time.sleep(2)  # Wait for page to load TODO make more intelligent waiting based on the driver itself
 
-    # Get source code and find all cards with price element
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    table = soup.find_all('div', attrs={'class': 'listingCardPrice'})
+def scrape(city_name: str) -> CityData:
+    city = CityData(city_name)
 
-    # Print price and address (address is two siblings away from price)
-    # TODO store this information
-    # TODO filter out land
-    for entry in table:
-        print(entry.text)
-        print(entry.next_sibling.next_sibling.text)
-        print(count)
-        count += 1
+    # Open driver on first page
+    driver = webdriver.Chrome()
+    driver.get(url=get_url(pg=1))
 
-pass
+    char = ''
+    while char != 'c':
+        char = input("Enter 'c' when captcha complete: ")
+    pass  # Breakpoint here to pass captcha TODO dynamically detect when captcha completed
+
+    count = 1  # How many houses have been found
+    page = 1
+    num_pages = 1
+    found_num_pages = False
+
+    # Go through pages
+    while page <= num_pages:
+        driver.get(url=get_url(pg=page))
+        driver.refresh()
+        time.sleep(2)  # Wait for page to load TODO make more intelligent waiting based on the driver itself
+
+        # Get source code
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+        # Read max number of pages and set the loop limit
+        if not found_num_pages:
+            num_pages = find_num_pages(sp=soup)
+            found_num_pages = True
+
+
+        # Find all cards with price element
+        entries_on_page = soup.find_all('div', attrs={'class': 'listingCardPrice'})
+
+        # Print price and address (address is two siblings away from price)
+        # TODO store this information
+        # TODO filter out land
+        for price_entry in entries_on_page:
+            city.add_house(address=price_entry.next_sibling.next_sibling.text, price=price_entry.text)
+
+            print(price_entry.text)
+            print(price_entry.next_sibling.next_sibling.text)
+            print(count)
+            count += 1
+
+        page += 1
+
+    driver.quit()
+    return city
+
+# TODO make this a function
+# TODO allow for different cities to be inputted
+command = ''
+while command != 'q':
+    command = input('Enter command: ')
+    match command:
+        case 's':
+            halifax = scrape(city_name="Halifax")
+            pass
+        case 'p':
+            f = open('halifax', 'wb')
+            if halifax:
+                pickle.dump(halifax, f)
+            f.close()
+            pass
+        case 'u':
+            f = open('halifax', 'rb')
+            read_data = pickle.load(f)
+            f.close()
+            if read_data:
+                for house in read_data.house_data:
+                    print(house)
+            pass
